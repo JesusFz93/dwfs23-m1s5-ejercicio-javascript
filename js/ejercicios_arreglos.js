244bf6fab7e900ca1bea63df0c56d11ddcbeb85d
Jesus <jesus.fdz93@gmail.com>
2023-10-14 14:15:32 -0500

    ejercicio 2

--- js/ejercicios_arreglos.js
+++ js/ejercicios_arreglos.js
@@ -8,19 +8,38 @@ promedio([70, 80, 80, 90, 60]) // Aprobado: 76.
 promedio([70, 50, 75, 70, 60]) // No Aprobado: 65.
 */
 
-const promedio = (calificaciones) => {
-  const sumatoria = calificaciones.reduce((acumulado, calificacion) => {
-    return acumulado + calificacion;
+// const promedio = (calificaciones) => {
+//   const sumatoria = calificaciones.reduce((acumulado, calificacion) => {
+//     return acumulado + calificacion;
+//   });
+
+//   const promedio = sumatoria / calificaciones.length;
+
+//   if (promedio >= 70) {
+//     console.log(`Aprobado: ${promedio}.`);
+//   } else {
+//     console.log(`No Aprobado: ${promedio}.`);
+//   }
+// };
+
+// promedio([70, 80, 80, 90, 60]);
+// promedio([70, 50, 75, 70, 60]);
+
+/*
+Ejercicio 2
+Método que reciba un array e imprima en la consola cada uno de sus elementos.
+
+imprimeArray([‘uno’, 2, null, 0]);
+	uno
+	2
+	null
+	0
+*/
+
+const imprimeArray = (arreglo) => {
+  arreglo.forEach((elemento) => {
+    console.log(elemento);
   });
-
-  const promedio = sumatoria / calificaciones.length;
-
-  if (promedio >= 70) {
-    console.log(`Aprobado: ${promedio}.`);
-  } else {
-    console.log(`No Aprobado: ${promedio}.`);
-  }
 };
 
-promedio([70, 80, 80, 90, 60]);
-promedio([70, 50, 75, 70, 60]);
+imprimeArray(["uno", 2, null, 0]);
